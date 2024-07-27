5e40cfc25fb6cd1ba526cc482e0af568b4448a9b
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-07-27 12:23:20 +0900

    .

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # binc
 
 binc is a utility to transparently compile updated source code into BINary and Cache it. 
+
--- lib/main.go
+++ lib/main.go
@@ -35,6 +35,8 @@ type MyDep interface {
 
 const appBase = "binc"
 
+var copiedBase = fmt.Sprintf(".%s", appBase)
+
 func iterateOverManagers(
 	fn func(manager common.Manager) error,
 	lastError error,
@@ -143,7 +145,7 @@ func recreateLinks() (err error) {
 			continue
 		}
 		linkPath := filepath.Join(linksDirPath, dirEntry.Name())
-		if V(os.Readlink(linkPath)) != appBase {
+		if V(os.Readlink(linkPath)) != copiedBase {
 			continue
 		}
 		V0(os.Remove(linkPath))
@@ -154,9 +156,6 @@ func recreateLinks() (err error) {
 		func(manager common.Manager) (err error) {
 			defer Catch(&err)
 			for _, commandBaseInfo := range manager.GetCommandBaseInfoList() {
-				if commandBaseInfo.CmdBase == appBase {
-					continue
-				}
 				linkPath := filepath.Join(linksDirPath, commandBaseInfo.CmdBase)
 				if _, err := os.Lstat(linkPath); err == nil {
 					println("Conflicting source:", commandBaseInfo.SourcePath)
@@ -167,7 +166,7 @@ func recreateLinks() (err error) {
 				if err != nil && !os.IsNotExist(err) {
 					return err
 				}
-				V0(os.Symlink(appBase, linkPath))
+				V0(os.Symlink(copiedBase, linkPath))
 			}
 			return nil
 		},
@@ -198,52 +197,48 @@ func execute(args []string, shouldRebuild bool) (err error) {
 }
 
 // install installs the given binary to the “links” directory.
-func install(path string) (err error) {
-	var binaryTypes = []types.Type{
-		matchers.TypeElf,
-		matchers.TypeMachO,
-		matchers.TypeExe,
-	}
-	defer Catch(&err)
-	absPath := V(filepath.Abs(path))
-	stat := V(os.Stat(absPath))
+func installSelfToLinksDir() (err error) {
+	cmdPath := V(filepath.EvalSymlinks(V(filepath.Abs(V(os.Executable())))))
+	destPath := filepath.Join(V(common.LinksDirPath()), copiedBase)
+	stat := V(os.Stat(cmdPath))
 	if stat.IsDir() {
 		return errors.New("not a file")
 	}
 	fileType := (func() types.Type {
-		in := V(os.Open(absPath))
+		in := V(os.Open(cmdPath))
 		defer (func() { Ignore(in.Close()) })()
 		return V(filetype.MatchReader(in))
 	})()
-	if !slices.Contains(binaryTypes, fileType) {
+	if !slices.Contains([]types.Type{
+		matchers.TypeElf,
+		matchers.TypeMachO,
+		matchers.TypeExe,
+	}, fileType) {
 		return errors.New("not an executable binary")
 	}
-	tempDestPath := filepath.Join(V(common.LinksDirPath()), appBase+".temp")
 	(func() {
-		out := V(os.Create(tempDestPath))
+		out := V(os.Create(destPath))
 		defer (func() { Ignore(out.Close()) })()
-		in := V(os.Open(absPath))
+		in := V(os.Open(cmdPath))
 		defer (func() { Ignore(in.Close()) })()
-		_ = V(io.Copy(out, in))
+		V0(io.Copy(out, in))
 	})()
-	V0(os.Chmod(tempDestPath, 0755))
-	destPath := filepath.Join(V(common.LinksDirPath()), appBase)
-	Ignore(os.Rename(tempDestPath, destPath))
-	V0(os.Rename(tempDestPath, destPath))
+	V0(os.Chmod(destPath, 0755))
 	return nil
 }
 
 func Main(args []string) (err error) {
-	defer Catch(&err)
-	//Debugger()
+	Debugger()
 	shouldRebuild := os.Getenv("BUILD") != "" || os.Getenv("REBUILD") != ""
-	if filepath.Base(args[0]) != appBase && // As symlink.
-		// GoLand run configuration workaround
+	// Run the target command.
+	if !slices.Contains([]string{appBase, copiedBase}, filepath.Base(args[0])) &&
+		// GoLand “run configuration” workaround
 		!strings.HasSuffix(args[0], "_"+appBase) {
-		commandArgs := args
-		return execute(commandArgs, shouldRebuild)
+		return execute(args, shouldRebuild)
 	}
-	if len(args[1:]) == 0 { // Without subcommand.
+	// Run binc command itself.
+	// No subcommand specified.
+	if len(args[1:]) == 0 {
 		return recreateLinks()
 	}
 	switch args[1] {
@@ -251,7 +246,11 @@ func Main(args []string) (err error) {
 		commandArgs := args[2:]
 		return execute(commandArgs, shouldRebuild)
 	case "install":
-		return install(args[0])
+		err = installSelfToLinksDir()
+		if err != nil {
+			return
+		}
+		return recreateLinks()
 	case "which":
 		return which(args[2])
 	}
